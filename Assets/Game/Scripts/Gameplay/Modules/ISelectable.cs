7c1907ca68b08b7d18f46a8741d348276a0f3393
Stan Graafmans <stan_live@hotmail.nl>
2017-12-07 13:37:38 +0100

    Add base selecting class

---
+++ Assets/Game/Scripts/Gameplay/Modules/ISelectable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AnotherRTS.Gameplay.Modules
+{
+	public interface ISelectable
+	{
+		bool IsSelected { get; set; }
+	}
+}
---
+++ Assets/Game/Scripts/Gameplay/Selector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UCamera = UnityEngine.Camera;
+using AnotherRTS.Gameplay.Modules;
+
+namespace AnotherRTS.Gameplay
+{
+	public class Selector : MonoBehaviour
+	{
+		private UCamera m_Camera;
+
+		[SerializeField] private LayerMask m_SelectionLayers;
+
+		private void Start()
+		{
+			m_Camera = UCamera.main;
+		}
+
+		private void Update()
+		{
+			if (Input.GetMouseButtonUp(0)) {
+				Vector2 mousePosition = Input.mousePosition;
+				RaycastHit hitInfo;
+				bool hitSuccess = Physics.Raycast(m_Camera.ScreenPointToRay(mousePosition), out hitInfo, m_Camera.farClipPlane, m_SelectionLayers);
+
+				if (hitSuccess) {
+					ISelectable selectable = hitInfo.collider.GetComponent<ISelectable>();
+					if (selectable != null) {
+						selectable.IsSelected = !selectable.IsSelected;
+					}
+				}
+			}
+		}
+	}
+}
