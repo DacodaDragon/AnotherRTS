d5c88ac1f758ae78fcd07ec3227306239df1a1fb
Errorcodebin <errorcodebin_2@hotmail.com>
2017-12-09 14:18:16 +0100

    Seperated InputManager.cs in standalone scripts

---
+++ Assets/Game/Scripts/Input/DataStructures/ControlGroup.cs
@@ -0,0 +1,27 @@
+﻿namespace AnotherRTS.Management.RemappableInput.IO
+{
+    public struct ControlGroup
+    {
+        public readonly string name;
+        public readonly Key[] keys;
+
+        public ControlGroup(string name, Key[] keys)
+        {
+            this.name = name;
+            this.keys = keys;
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            sb.Append(name);
+            sb.Append('\n');
+            for (int i = 0; i < keys.Length; i++)
+            {
+                sb.Append(keys[i].ToString());
+                sb.Append('\n');
+            }
+            return sb.ToString();
+        }
+    }
+}
