8cd9e37cecb80244d860b3dcb0c3dae043dfe098
Errorcodebin <errorcodebin_2@hotmail.com>
2017-12-10 12:29:43 +0100

    Re-organizing Input folder

---
+++ Assets/Game/Scripts/Input/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Click!");
+        }
+    }
+}
