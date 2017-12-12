e7cfee82939165354206d2e34359a31587a74830
Errorcodebin <errorcodebin_2@hotmail.com>
2017-12-12 09:46:40 +0100

    Better Input Testing

--- Assets/Game/Scripts/Input/Tests/InputTester.cs
+++ Assets/Game/Scripts/Input/Tests/InputTester.cs
@@ -5,25 +5,48 @@ using UnityEngine;
 public class InputTester : MonoBehaviour {
 
     [SerializeField]
-    string name;
-    int id;
+    TestKey[] keys;
 
     [SerializeField]
     InputManager manager;
 
 	// Use this for initialization
 	void Start () {
-        id = manager.GetKeyID(name);
-        Debug.Log("ID is : " + id);
+        for (int i = 0; i < keys.Length; i++)
+        {
+            keys[i].Init(manager);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (manager.GetKeyDown(id))
-            Debug.Log("Pressed!");
-        if (manager.GetKey(id))
-            Debug.Log("Held!");
-        if (manager.GetKeyUp(id))
-            Debug.Log("Released!");
+        for (int i = 0; i < keys.Length; i++)
+        {
+            keys[i].Update(manager);
+        }
+    }
+
+    [System.Serializable]
+    public class TestKey
+    {
+        [SerializeField]
+        string name;
+        int id;
+
+        public void Init(InputManager manager)
+        {
+            id = manager.GetKeyID(name);
+            Debug.Log(name + " has ID: " + id);
+        }
+
+        public void Update(InputManager manager)
+        {
+            if (manager.GetKeyDown(id))
+                Debug.Log(name + " Pressed!");
+            if (manager.GetKey(id))
+                Debug.Log(name + " Held!");
+            if (manager.GetKeyUp(id))
+                Debug.Log(name + " Released!");
+        }
     }
 }
