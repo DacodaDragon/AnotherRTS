fe4a995deefb5acaed795f5302050ee79e047c00
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-13 19:50:24 +0100

    Fixed Structure not implementing ISelectable

--- Assets/Game/Scripts/Gameplay/Entities/Structure.cs
+++ Assets/Game/Scripts/Gameplay/Entities/Structure.cs
@@ -1,8 +1,17 @@
-﻿
+﻿using System;
+
 namespace AnotherRTS.Gameplay.Entities
 {
-	public class Structure : Entity, ISelectable
-	{
+    public class Structure : Entity, ISelectable
+    {
+        public void OnEntityDeselect()
+        {
+            throw new NotImplementedException();
+        }
 
-	}
+        public void OnEntitySelect()
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
