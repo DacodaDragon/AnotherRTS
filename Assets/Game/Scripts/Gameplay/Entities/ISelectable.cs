0ebd89b28c68da5e822b282bb9171e0b5a5256b7
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-13 16:24:36 +0100

    Modified ISelectable to contain functionality
    OnSelect so the unit can respond to a selection
    OnDeselect so the unit can respond to being deselected

--- Assets/Game/Scripts/Gameplay/Entities/ISelectable.cs
+++ Assets/Game/Scripts/Gameplay/Entities/ISelectable.cs
@@ -3,6 +3,7 @@ namespace AnotherRTS.Gameplay.Entities
 {
 	public interface ISelectable
 	{
-
+        void OnEntitySelect();      // gets called when the entity is selected
+        void OnEntityDeselect();    // gets called when the entity is deselected
 	}
 }
