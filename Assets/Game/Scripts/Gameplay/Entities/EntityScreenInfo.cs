4cdaa2c250619bffa5c67cc779fec07a5430a87b
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-13 18:39:10 +0100

    Added EntityScreenInfo

---
+++ Assets/Game/Scripts/Gameplay/Entities/EntityScreenInfo.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace AnotherRTS.Gameplay.Entities.Units
+{
+    public struct EntityScreenInfo<ContextType>
+    {
+        public readonly Vector2 position;
+        public readonly ContextType context;
+
+        public EntityScreenInfo(Vector2 pos, ContextType context )
+        {
+            this.position = pos;
+            this.context = context;
+        }
+    }
+}
