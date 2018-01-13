4a096a08a076d17565120287cb1bdb4219e03205
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-13 16:22:08 +0100

    Modified Unit to inherit Interfaces instead of derived classes

--- Assets/Game/Scripts/Gameplay/Entities/Unit.cs
+++ Assets/Game/Scripts/Gameplay/Entities/Unit.cs
@@ -1,8 +1,30 @@
-﻿
+﻿using AnotherRTS.Util.Notification;
+using Logger = BoneBox.Debug.Logger;
+
 namespace AnotherRTS.Gameplay.Entities
 {
-	public class Unit : Entity, ISelectable
-	{
+    public class Unit : Entity, ICommandableEntity<Unit>, IDamagable, IDestroyable, IDamageNotification<Unit>, IDeathNotification<Unit>, ISelectable
+    {
+        private ITaskManager<Unit> m_TaskManager = null;
+        public ITaskManager<Unit> TaskManager { get { return m_TaskManager; } protected set { m_TaskManager = value; } }
+        public float Health { get; protected set; }
+        public float MaxHealth { get; protected set; }
+        public event NotifyChange<Unit, float> OnDamageRecieved;
+        public event Notify<Unit> OnDeath;
 
-	}
+        void IDamagable.Damage(float amount) { Damage(amount); }
+        void IDestroyable.Destroy() { Destroy(); }
+        void ISelectable.OnEntityDeselect() { OnEntityDeselect(); }
+        void ISelectable.OnEntitySelect() { OnEntitySelect(); }
+
+        public virtual void Damage(float amount) { Logger.Log(this, "Recieved Damage! " + amount); }
+        public virtual void Destroy() { Logger.Log(this, "Got Destroyed!"); }
+        public virtual void OnEntityDeselect() { Logger.Log(this, "Was Deselected!"); }
+        public virtual void OnEntitySelect() { Logger.Log(this, "Was Selected!"); }
+
+        private void Start()
+        {
+            Type = EType.Unit;
+        }
+    }
 }
--- Assets/Game/Scripts/Gameplay/Entities/Unit/TestUnit.cs
+++ Assets/Game/Scripts/Gameplay/Entities/Unit/TestUnit.cs
@@ -2,39 +2,11 @@
 using System;
 using AnotherRTS.Util.Notification;
 
-namespace AnotherRTS.Gameplay.Entities.Unit
+namespace AnotherRTS.Gameplay.Entities.Units
 {
-    public class TestUnit : Unit, ICommandableEntity<Unit>, IDamagable, IDestroyable, IDamageNotification<Unit>, IDeathNotification<Unit>, ISelectable
+    public class TestUnit : Unit
     {
-        private ITaskManager<Unit> m_TaskManager;
-        private float m_health;
-        private float m_maxhealth;
-        public ITaskManager<Unit> TaskManager{ get { return m_TaskManager; } }
-        public float Health { get; }
-        public float MaxHealth { get; }
-        public event NotifyChange<Unit, float> OnDamageRecieved;
-        public event Notify<Unit> OnDeath;
 
-        private void Start()
-        {
-            m_TaskManager = new StandardTaskManager();
-            m_TaskManager.TaskRun((Unit)this);
-        }
-
-        private void Update()
-        {
-            m_TaskManager.TaskRun((Unit)this);
-        }
-
-        public void Damage(float amount)
-        {
-            OnDamageRecieved.Invoke((Unit)this, amount);
-        }
-
-        public void Destroy()
-        {
-            OnDeath.Invoke((Unit)this);
-        }
     }
 }
 
