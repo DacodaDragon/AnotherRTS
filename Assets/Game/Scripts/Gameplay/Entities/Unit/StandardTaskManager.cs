284a52f9d8d3f84435085cb7da5e53ba51e0e6e2
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-14 13:30:27 +0100

    Fixed First added task not being executed

--- Assets/Game/Scripts/Gameplay/Entities/Unit/StandardTaskManager.cs
+++ Assets/Game/Scripts/Gameplay/Entities/Unit/StandardTaskManager.cs
@@ -19,6 +19,8 @@ namespace AnotherRTS.Gameplay.Entities.Units
         public void TaskAdd(ITask<Unit> Task)
         {
             Tasks.Add(Task);
+            if (Tasks.Count == 1)
+                Tasks[0].StartTask(contextUnit);
         }
 
         public void TaskAssign(ITask<Unit> Task)
