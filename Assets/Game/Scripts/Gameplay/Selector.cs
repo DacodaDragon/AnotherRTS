d17e263847eabbfd66277ae148940db880f07eaf
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-14 12:03:56 +0100

    Modifier Selector to temporarily send move commands on mouse1

--- Assets/Game/Scripts/Gameplay/Selector.cs
+++ Assets/Game/Scripts/Gameplay/Selector.cs
@@ -8,73 +8,106 @@ using Logger = BoneBox.Debug.Logger;
 
 using AnotherRTS.Management.RemappableInput;
 using AnotherRTS.Gameplay.Entities;
+using System;
 
 namespace AnotherRTS.Gameplay
 {
-	public class Selector : Singleton<Selector>
-	{
-		private UCamera m_Camera;
-		private InputManager m_InputManager;
-		private int m_SingleSelectKey;
-		private int m_MultiSelectKey;
-		private List<ISelectable> m_SelectedEntities;
+    public partial class Selector : Singleton<Selector>
+    {
+        private UCamera m_Camera;
+        private InputManager m_InputManager;
+        private int m_SingleSelectKey;
+        private int m_MultiSelectKey;
 
-		[SerializeField] private LayerMask m_SelectionLayers;
+        // TODO: Move commankey to commandable
+        private int m_CommandKey;
 
-		public List<ISelectable> SelectedEntities { get { return m_SelectedEntities; } }
+        private List<ISelectable> m_SelectedEntities;
 
-		private new void Awake()
-		{
-			base.Awake();
-			m_SelectedEntities = new List<ISelectable>();
-		}
+        [SerializeField] private LayerMask m_SelectionLayers;
 
-		private void Start()
-		{
-			m_Camera = UCamera.main;
-			m_InputManager = InputManager.Instance;
-			m_SingleSelectKey = m_InputManager.GetKeyID("single select");
-			m_MultiSelectKey = m_InputManager.GetKeyID("multi select");
-		}
+        public List<ISelectable> SelectedEntities { get { return m_SelectedEntities; } }
 
-		private void TrySelect()
-		{
-			Vector2 mousePosition = Input.mousePosition;
-			RaycastHit hitInfo;
-			bool hitSuccess = Physics.Raycast(m_Camera.ScreenPointToRay(mousePosition), out hitInfo, m_Camera.farClipPlane, m_SelectionLayers);
+        private new void Awake()
+        {
+            base.Awake();
+            m_SelectedEntities = new List<ISelectable>();
+        }
 
-			if (hitSuccess)
-			{
-				ISelectable selectable = hitInfo.collider.GetComponent<ISelectable>();
-				if (selectable != null)
-				{
-					if (!m_SelectedEntities.Contains(selectable))
-					{
-						m_SelectedEntities.Add(selectable);
-						Logger.Log(this, $"Selected {selectable.ToString()}");
-					}
-				}
-			}
-		}
+        private void Start()
+        {
+            m_Camera = UCamera.main;
+            m_InputManager = InputManager.Instance;
+            m_SingleSelectKey = m_InputManager.GetKeyID("single select");
+            m_MultiSelectKey = m_InputManager.GetKeyID("multi select");
+            m_CommandKey = m_InputManager.GetKeyID("unit move");
+            m_SelectionLayers.value = LayerMask.GetMask("Unit");
+        }
 
-		public void DeselectAll()
-		{
-			m_SelectedEntities.Clear();
-			Logger.Log(this, "Deselected all");
-		}
+        private void TrySelect()
+        {
+            Vector2 mousePosition = Input.mousePosition;
+            RaycastHit hitInfo;
+            bool hitSuccess = Physics.Raycast(m_Camera.ScreenPointToRay(mousePosition), out hitInfo, m_Camera.farClipPlane, m_SelectionLayers);
 
-		private void Update()
-		{
-			if (m_InputManager.GetKeyUp(m_SingleSelectKey))
-			{
-				DeselectAll();
-				TrySelect();
-			}
+            if (hitSuccess)
+            {
+                ISelectable selectable = hitInfo.collider.GetComponent<ISelectable>();
 
-			if (m_InputManager.GetKeyUp(m_MultiSelectKey))
-			{
-				TrySelect();
-			}
-		}
-	}
+                if (selectable != null)
+                {
+                    if (!m_SelectedEntities.Contains(selectable))
+                    {
+                        m_SelectedEntities.Add(selectable);
+                        selectable.OnEntitySelect();
+                        Logger.Log(this, $"Selected {selectable.ToString()}");
+                    }
+                }
+            }
+        }
+
+        public void DeselectAll()
+        {
+            for (int i = 0; i < m_SelectedEntities.Count; i++)
+            {
+                m_SelectedEntities[i].OnEntityDeselect();
+            }
+            m_SelectedEntities.Clear();
+            Logger.Log(this, "Deselected all");
+        }
+
+        private void Update()
+        {
+            if (m_InputManager.GetKeyUp(m_SingleSelectKey))
+            {
+                DeselectAll();
+                TrySelect();
+            }
+
+            if (m_InputManager.GetKeyUp(m_MultiSelectKey))
+            {
+                TrySelect();
+            }
+
+            // TODO: Move commankey to commandable
+            if (m_InputManager.GetKeyUp(m_CommandKey))
+            {
+                Vector2 mousePosition = Input.mousePosition;
+                RaycastHit hitInfo;
+                bool hitSuccess = Physics.Raycast(m_Camera.ScreenPointToRay(mousePosition), out hitInfo, m_Camera.farClipPlane, LayerMask.GetMask("Default"));
+
+                if (hitSuccess)
+                {
+                    for (int i = 0; i < m_SelectedEntities.Count; i++)
+                    {
+                        if (m_SelectedEntities[i] is ICommandableEntity<Unit>)
+                        {
+                            ((ICommandableEntity<Unit>)m_SelectedEntities[i])
+                                .TaskManager.TaskForce(new MoveTask(hitInfo.point));
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
