3d33907b292c7fddfd313ee10d2572d08db236b9
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-14 12:03:14 +0100

    Added BasicPhysicsMovementController

---
+++ Assets/Game/Scripts/Gameplay/Entities/Unit/BasicPhysicsMovementController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace AnotherRTS.Gameplay.Entities.Units
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class BasicPhysicsMovementController : MonoBehaviour, IMovementController
+    {
+        NavMeshAgent aStar;
+
+        public void Awake()
+        {
+            aStar = GetComponent<NavMeshAgent>();
+        }
+
+        public void Follow(Transform target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveTowards(Vector3 target)
+        {
+            aStar.SetDestination(target);
+
+        }
+
+        public void MoveTowards(Transform target)
+        {
+            aStar.SetDestination(target.position);
+
+        }
+    }
+}
+
