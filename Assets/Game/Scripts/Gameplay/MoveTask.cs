88f1b37c49c62720a3ff64ae3c9a77b4ce0832fe
Errorcodebin <errorcodebin_2@hotmail.com>
2018-01-14 12:02:38 +0100

    Added MoveTask

---
+++ Assets/Game/Scripts/Gameplay/MoveTask.cs
@@ -0,0 +1,49 @@
+﻿
+using UnityEngine;
+
+using BoneBox.Core;
+using AnotherRTS.Gameplay.Entities;
+
+namespace AnotherRTS.Gameplay
+{
+    public partial class Selector : Singleton<Selector>
+    {
+        public class MoveTask : ITask<Unit>
+        {
+            public string TaskName { get { return "Move Unit"; } }
+
+            private Vector3 targetposition;
+
+            public ETaskRequirement[] TaskRequirements
+            {
+                get
+                {
+                    return new ETaskRequirement[]
+                    { ETaskRequirement.CanMove };
+                }
+            }
+
+            public void EndTask(Unit context)
+            {
+
+            }
+
+            public void RunTask(Unit context)
+            {
+
+            }
+
+            public void StartTask(Unit context)
+            {
+                ((IMovable)context)
+                    .MovementController
+                    .MoveTowards(targetposition);
+            }
+
+            public MoveTask(Vector3 position)
+            {
+                this.targetposition = position;
+            }
+        }
+    }
+}
